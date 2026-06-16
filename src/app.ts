e16051bf825a6e1efb319c2bd7820aa42c5f899c
practiceTaqwa <muhammadabuhafz@gmail.com>
2026-06-16 16:55:00 +0600

    update post api created

--- src/app.ts
+++ src/app.ts
@@ -4,51 +4,93 @@ import { prisma } from "./lib/prisma";
 const app = express();
 export const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post('/post',async(req,res)=>{
+app.post("/post", async (req, res) => {
+  const { title, description } = req.body;
 
-    const {title,description}=req.body;
+  try {
+    const result = await prisma.post.create({
+      data: {
+        title,
+        description,
+      },
+    });
 
-    try {
-        const result=await prisma.post.create({
-            data:{
-                title,
-                description
-            }
-        })
+    res.status(201).json({
+      success: true,
+      message: "post created sucessfully",
+      data: result,
+    });
+  } catch (error) {}
+});
 
-        res.status(201).json({
-            success:true,
-            message:'post created sucessfully',
-            data:result
-        })
+app.get("/post", async (req, res) => {
+  try {
+    const result = await prisma.post.findMany();
 
-    } catch (error) {
-        
-    }
-})
+    res.status(201).json({
+      success: true,
+      message: "post created sucessfully",
+      data: result,
+    });
+  } catch (error) {}
+});
+app.get("/post/:id", async (req, res) => {
+  try {
+    const result = await prisma.post.findUniqueOrThrow({
+      where: {
+        id: "01KV80D0474GGN5CZH1C32F3S7",
+      },
+    });
 
-app.get('/post/:id',async(req,res)=>{
-        try {
-        const result=await prisma.post.findMany({
-            where:{
-                id:'01KV80D0474GGN5CZH1C32F3S7'
-            }
-        })
+    res.status(201).json({
+      success: true,
+      message: "post created sucessfully",
+      data: result,
+    });
+  } catch (error) {}
+});
 
-        res.status(201).json({
-            success:true,
-            message:'post created sucessfully',
-            data:result
-        })
+app.delete("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await prisma.post.delete({
+      where: {
+        id,
+      },
+    });
 
-    } catch (error) {
-        
-    }
-})
+    res.status(201).json({
+      success: true,
+      message: "post deleted sucessfully",
+      data: result,
+    });
+  } catch (error) {}
+});
+app.patch("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+  try {
+    const result = await prisma.post.update({
+      where: {
+        id,
+      },
+      data: {
+        title: title,
+        description: description,
+      },
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "post deleted sucessfully",
+      data: result,
+    });
+  } catch (error) {}
+});
 
 export default app;
